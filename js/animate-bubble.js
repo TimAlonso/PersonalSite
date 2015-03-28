00d6ad5e6a0ba6b3a3986e0a604e8ddec14cb913
Tim Alonso <tim.alonso@outlook.com>
2015-03-28 12:32:34 +0000

    Reverted back to the original design. I can add additional animation on hover instead of focusing on position.

--- js/animate-bubble.js
+++ js/animate-bubble.js
@@ -1,39 +1,39 @@
-jQuery.fn.verticalMarquee = function(vertSpeed, horiSpeed, index) {
+// jQuery.fn.verticalMarquee = function(vertSpeed, horiSpeed, index) {
    
-    this.css('float', 'left');
+//     this.css('float', 'left');
 
-    vertSpeed = vertSpeed || 1;
-    horiSpeed = 1/horiSpeed || 1;
+//     vertSpeed = vertSpeed || 1;
+//     horiSpeed = 1/horiSpeed || 1;
 
-    var windowH = this.parent().height(),
-        thisH = this.height(),
-        parentW = (this.parent().width() - this.width()) / 2,
-        rand = Math.random() * (index * 1000),
-        current = this;
+//     var windowH = this.parent().height(),
+//         thisH = this.height(),
+//         parentW = (this.parent().width() - this.width()) / 2,
+//         rand = Math.random() * (index * 1000),
+//         current = this;
 
-    this.css('margin-top', windowH + thisH);
-    this.parent().css('overflow', 'hidden');
+//     this.css('margin-top', windowH + thisH);
+//     this.parent().css('overflow', 'hidden');
 
-    setInterval(function() {
-        current.css({
-            marginTop: function(n, v) {
-                return parseFloat(v) - vertSpeed;
-            },
-            marginLeft: function(n, v) {
-                return (Math.sin(new Date().getTime() / (horiSpeed * 1000) + rand) + 1) * parentW;
-            }
-        });
-    }, 15);
+//     setInterval(function() {
+//         current.css({
+//             marginTop: function(n, v) {
+//                 return parseFloat(v) - vertSpeed;
+//             },
+//             marginLeft: function(n, v) {
+//                 return (Math.sin(new Date().getTime() / (horiSpeed * 1000) + rand) + 1) * parentW;
+//             }
+//         });
+//     }, 15);
 
-    setInterval(function() {
-        if (parseFloat(current.css('margin-top')) < -thisH) {
-            current.css('margin-top', windowH + thisH);
-        }
-    }, 250);
-};
+//     setInterval(function() {
+//         if (parseFloat(current.css('margin-top')) < -thisH) {
+//             current.css('margin-top', windowH + thisH);
+//         }
+//     }, 250);
+// };
 
-var bubbleIndex = 1;
-$('.bubble').each(function(bubbleIndex) {  
-    $(this).verticalMarquee(1, 1, bubbleIndex);
-    bubbleIndex++
-});
+// var bubbleIndex = 1;
+// $('.bubble').each(function(bubbleIndex) {  
+//     $(this).verticalMarquee(1, 1, bubbleIndex);
+//     bubbleIndex++
+// });
