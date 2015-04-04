19773a15d87abdf6cf7f9789299d16ba3efe3c97
Tim Alonso <tim.alonso@outlook.com>
2015-04-04 22:10:15 +0100

    Playing around with IIS to test HTTP request and not get pestered with cross origin requests are only supported for HTTP error.

--- js/bubble-graph.js
+++ js/bubble-graph.js
@@ -1,18 +1,29 @@
+// Retrieve content from the API
+// Parse content into JSON
+// Retrieve relevant data from JSON objects
+
+// Sample Query for retrieving java and c# keyword and occurances from specific date
+// trendyskills.com/service?q=keywordDate&keyID[]=915&keyID[]=691&dateFrom=2013/02/17&dateTo=2013/02/24&key=###
+
+// This query will only retrieve languages (as opposed to all sort of skills) and count the occurances of these languages. I may have to select the top 10 (if possible) and render these depending on the size.
+
+// Get all of the keywords available. This is a massive list. Data represented is the id and keyname.
+
+// Use KeywordDate to display all of the keywords's occurances within a date. Current and the last year.
+
 var baseUrl = "trendyskills.com/service";
 var apiKey = "###";
 
 var xhr = new XMLHttpRequest();
 
-var categoryQuery = baseUrl + "q=categories&" + "key=";
-var query = baseUrl + "q=status&" + "key=" + apiKey;
+var query = baseUrl + "?q=keywords&key=" + apiKey;
 
 xhr.open("GET", query, false);
 xhr.send();
 
-function getCategories(query, apiKey) {
-	var query = categoryQuery + apiKey;
-	xhr.open("GET", query, false);
-	var request = xhr.send();
+var keywords = JSON.parse(request.responseText);
 
-	return categories = JSON.parse(request.responseText);
-}
+console.log("Test");
+
+// Can't test locally. I don't want to push with my api key.
+// Trying to create a website in IIS locally so that I can test this Javascript.
